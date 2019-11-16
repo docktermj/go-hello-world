d7e4dc7d5a14c158e087686d2846939f58b3c960
Michael Dockter <michael@dockter.com>
2019-11-16 11:31:55 -0500

    issue-3 Tidy up

--- hello.go
+++ hello.go
@@ -1,9 +1,7 @@
 package hello
 
 import (
-	//	"fmt"
-	//	"rsc.io/quote"
-	"rsc.io/quote/v3"
+	"fmt"
 )
 
 // Values updated via "go install -ldflags" parameters.
@@ -13,10 +11,5 @@ var buildVersion string = "0.0.0"
 var buildIteration string = "0"
 
 func Hello() string {
-	//	return fmt.Sprintf("Hello, world! from %s version %s-%s\n", programName, buildVersion, buildIteration)
-	return quote.HelloV3()
-}
-
-func Proverb() string {
-	return quote.Concurrency()
+	return fmt.Sprintf("Hello, world! from %s version %s-%s\n", programName, buildVersion, buildIteration)
 }
--- hello_test.go
+++ hello_test.go
@@ -1,6 +1,7 @@
 package hello
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -8,17 +9,9 @@ import (
  * The unit tests in this file simulate command line invocation.
  */
 
-func TestHello(test *testing.T) {
-	//	want := "Hello, world! from unknown version 0.0.0-0\n"
-	want := "Hello, world."
-	if got := Hello(); got != want {
-		test.Errorf("Hello() = %q, want %q", got, want)
-	}
-}
-
-func TestProverb(t *testing.T) {
-	want := "Concurrency is not parallelism."
-	if got := Proverb(); got != want {
-		t.Errorf("Proverb() = %q, want %q", got, want)
+func TestHello(testing *testing.T) {
+	want := "Hello, world! from unknown version"
+	if got := Hello(); !strings.HasPrefix(got, want) {
+		testing.Errorf("Hello() = %q, want %q", got, want)
 	}
 }
